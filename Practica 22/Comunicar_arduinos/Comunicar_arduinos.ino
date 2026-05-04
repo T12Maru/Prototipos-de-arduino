5be5aa9d12eb9c3c77a9da690d5ce31a70c92401
t12_maru <cesarmvillalobos@hotmail.com>
2026-05-03 22:40:27 -0700

    Se modifico completamente el automata original de arduino, processing solo envia y recibe

--- Practica 22/Comunicar_arduinos/Comunicar_arduinos.ino
+++ Practica 22/Comunicar_arduinos/Comunicar_arduinos.ino
@@ -1,5 +1,7 @@
 #include <SoftwareSerial.h>
 
+#define MAX_MSG 65535
+
 // Variables globales para comunicación del arduino
 const int rxpin = 2;
 const int txpin = 3;
@@ -7,6 +9,10 @@ const int txpin = 3;
 // Variables que tienen que ver con el protocolo de comunicación
 const byte H = 'H';
 const byte F = 'F';
+
+bool recibiendo_mensaje = false;
+bool conozco_tamanio = false;
+byte [MAX_MSG] buffer; 
 
 int estado = 0;
 unsigned int size = 0;
@@ -30,77 +36,110 @@ void enviarArduino(unsigned int size){
   serialArduino.write(size & 0xFF);
   
   for(unsigned int i = 0 ; i < size ; i++){
-    unsigned long start = millis();
-
-    while(!Serial.available()){//si no ha terminado de llegar los datos
-      if(millis() - start > 5000){ // 5 segundos timeout por byte
-      Serial.println("#Timeout leyendo datos");
-      return;
-  }
-    } 
-    byte b = Serial.read();
-    serialArduino.write(b);
+    serialArduino.write(buffer[i]);
   }
 
   serialArduino.write(F);
 }
 
-void enviarPC(byte b){
-  switch(estado){
-      case 0: // El automata espera H
+void enviarPC(int size){
+  // bits mas significativos a menos significativos
+  serialArduino.write((size >> 8) & 0xFF);
+  //bits menos significativos
+  serialArduino.write(size & 0xFF);
+  
+  for(unsigned int i = 0 ; i < size ; i++){
+    serialArduino.write(buffer[i]);
+  }
+}
+
+void loop() {
+  if(Serial.available() > 0){ // Si la computadora a la cuál estoy conectado me envió algo:
+    switch(estado){
+      case 0; // Esperando H
+        byte b = Serial.read();
         if(b == H){
           estado = 1;
           size = 0;
           leidos = 0;
         }
         break;
-      
-      case 1: // El automata espera el tamaño de byte de los datos a recibir
-        size = (size << 8 ) | b;
-        leidos++;
-        
-        // cuando se leyeron los 2 bytes de tamaño, seguimos con el siguiente estado
-        if(leidos == 2){
-          leidos = 0;
-          estado = 2;
+
+      case 1; // Esperando el tamanio (corresponde a 2 bytes)
+        if(Serial.available() >= 2){// nos aseguramos de leer solo cuando estén los 2 bits del tamaño...
+          byte b = Serial.read();
+          size = (b << 8);
+          b = Serial.read();
+          size |= b;
+          estado = 2 ;
         }
         break;
-        
-      case 2: // El automata retransmite los datos recibidos
-        Serial.write(b);
-        leidos++;
-        
+      case 2; // esperamos el mensaje a transmitir
+        while(Serial.available() > 0 && (leidos < size)){
+          byte b = Serial.read();
+          buffer[leidos++] = b;
+        }
+
         if(leidos == size){
           estado = 3;
         }
+
         break;
 
-      case 3: // El automata espera la terminación del mensaje
+      case 3; // esperamos la F 
+        byte b = Serial.read();
         if(b == F){
-          Serial.println("#Archivo recibido completo");
-      }
-      estado = 0;
-      break;
+          enviarArduino(size);
+          estado = 0;
+          size = 0;
+          leidos = 0;
+        }
+        break;
     }
-}
-
-void loop() {
-  if(Serial.available() >= 2){
-    Serial.println("#e recibio el mensaje de processing en el arduino UNO\n");
-    byte b = Serial.read();
-    size = (b << 8);
-    b = Serial.read();
-    size |= b;
-
-    Serial.println("se enviara el mensaje de processing con el tamaño");
-    Serial.println(size);
-    enviarArduino(size);
-    Serial.println("se transmitio el mensaje de processing al arduino mega");
   }
 
   if(serialArduino.available() > 0){
-    byte b = serialArduino.read();
+    
+    switch(estado){
+      case 0; // Esperando H
+        byte b = serialArduino.read();
+        if(b == H){
+          estado = 1;
+          size = 0;
+          leidos = 0;
+        }
+        break;
 
-    enviarPC(b);
+      case 1; // Esperando el tamanio (corresponde a 2 bytes)
+        if(serialArduino.available() >= 2){// nos aseguramos de leer solo cuando estén los 2 bits del tamaño...
+          byte b = serialArduino.read();
+          size = (b << 8);
+          b = serialArduino.read();
+          size |= b;
+          estado = 2 ;
+        }
+        break;
+      case 2; // esperamos el mensaje a transmitir
+        while(serialArduino.available() > 0 && (leidos < size)){
+          byte b = serialArduino.read();
+          buffer[leidos++] = b;
+        }
+
+        if(leidos == size){
+          estado = 3;
+        }
+
+        break;
+
+      case 3; // esperamos la F 
+        byte b = serialArduino.read();
+        if(b == F){
+          enviarPC(size);
+          estado = 0;
+          size = 0;
+          leidos = 0;
+        }
+        break;
+    }
   }
 }
--- Practica 22/Interfaz_processing/Interfaz_processing.pde
+++ Practica 22/Interfaz_processing/Interfaz_processing.pde
@@ -1,7 +1,5 @@
 import processing.serial.*;
 
-
-
 Serial myPort;
 
 byte[] file;
@@ -12,7 +10,7 @@ int size = 0;
 int leidos = 0;
 boolean debug = true; 
 
-public static final short portIndex = 3;
+public static final short portIndex = 2;
 
 void setup(){
   println(Serial.list());
@@ -34,65 +32,24 @@ void keyPressed(){
 }
 
 void enviar_archivo(byte [] file){  
+
   println("Enviando archivo...");
 
   println("enviando: " + str(file.length));
+  
+  myPort.write('H');
   myPort.write((file.length >> 8) & 0xFF);
   myPort.write(file.length & 0xFF);
   
   for(int i = 0; i < file.length; i++){
     myPort.write(file[i]);
   }
+  myPort.write('F');
 }
 
 void serialEvent(Serial p){
   while(p.available() > 0){
-    int aux = p.read();
-    if(debug){
-      while(p.available() > 0){
-        aux = p.read();
-        print((char)aux);
-        if(aux =='\n') break;
-      }
-    }else{
-      byte b = (byte)aux;
-    
-      switch(estado){
-      
-      case 0:
-        size = (b & 0xFF) << 8;
-        estado = 1;
-        break;
-      
-      case 1:
-        size |= (b & 0xFF);
-        
-        if(size <= 0 || size > 4096){
-          println("Tamaño inválido: " + size);
-          estado = 0;
-          leidos = 0;
-          size = 0;
-          break;
-        }
-        
-        buffer = new byte[size];
-        leidos = 0;
-        estado = 2;
-        break;
-      
-      case 2:
-        buffer[leidos++] = b;
-      
-        if(leidos == size){
-          println("Archivo recibido");
-          saveBytes("recibido.bin", buffer);
-          estado = 0;
-        }
-        break;
-      case 3:
-        print("solo se leyo un mensaje de debug, no se procesa el switch");
-        break;
-      }
-    }
+    int data = p.read();
+    print("Recibi: " + binary(data,8));
   }
 }
